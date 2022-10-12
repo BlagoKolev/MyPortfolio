987ad3ff071fbdd80f3400b946a7c36f9bda4564
Blagovest <blagovest.kolev@gmail.com>
2022-10-12 11:52:59 +0300

    Finish the floating Nav component.

--- my-portfolio/src/components/nav/Nav.js
+++ my-portfolio/src/components/nav/Nav.js
@@ -1,14 +1,16 @@
 import './nav.css';
 import { AiOutlineHome, AiOutlineUser, AiOutlinePhone } from 'react-icons/ai';
 import { MdLaptopChromebook } from 'react-icons/md';
-
+import { useState} from 'react';
 
 function Nav() {
+    const [activeNav, setActiveNav] = useState('#');
+
     return (<nav>
-        <a href='#'><AiOutlineHome /></a>
-        <a href='#about'><AiOutlineUser /></a>
-        <a href='#experience'><MdLaptopChromebook /></a>
-        <a href='#contacts'><AiOutlinePhone /></a>
+        <a href='#' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
+        <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
+        <a href='#experience' onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><MdLaptopChromebook /></a>
+        <a href='#contacts' onClick={() => setActiveNav('#contacts')} className={activeNav === '#contacts' ? 'active' : ''}><AiOutlinePhone /></a>
     </nav>)
 }
 
