ff709bf7acc030bed5ce2a5b1a948f92a0a04b8d
Blagovest <blagovest.kolev@gmail.com>
2022-10-12 10:30:12 +0300

    Add github and linkedin links to portfolio.

--- my-portfolio/src/components/header/HeaderSocials.js
+++ my-portfolio/src/components/header/HeaderSocials.js
@@ -3,8 +3,8 @@ import { BsGithub, BsLinkedin } from 'react-icons/bs';
 function HeaderSocials() {
     return (
         <div className="header__socials">
-            <a href="" target="_blank"><BsGithub /></a>
-            <a href="" target="_blank"><BsLinkedin /></a>
+            <a href="https://github.com/BlagoKolev" target="_blank"><BsGithub /></a>
+            <a href="https://www.linkedin.com/in/blagovest-kolev-0021b3229/" target="_blank"><BsLinkedin /></a>
         </div>
     )
 }
