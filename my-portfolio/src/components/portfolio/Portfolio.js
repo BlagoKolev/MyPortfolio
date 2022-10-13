3edf03ade3cc5351e0250818427dc12c481c4ae3
Blagovest <blagovest.kolev@gmail.com>
2022-10-13 12:10:37 +0300

    Finished Portfolio component (Personal Projects).

--- my-portfolio/src/components/portfolio/Portfolio.js
+++ my-portfolio/src/components/portfolio/Portfolio.js
@@ -1,7 +1,55 @@
-import style from './Portfolio.module.css';
+import './portfolio.css';
+import forum from '../../assets/forum.png';
+import automarket from '../../assets/automarket.png';
+import bookshelf from '../../assets/bookshelf.png';
+import quiz from '../../assets/quiz.png';
 
 function Portfolio() {
+    return (
+        <section id='portfolio'>
+            <h5>
+                My Recent Work
+            </h5>
+            <h2>Portfolio</h2>
 
+            <div className='container portfolio__container'>
+
+                <article className='portfolio__item'>
+                    <div className='portfolio__item-image'>
+                        <img src={automarket} alt='automarket project image'></img>
+                    </div>
+                    <h3>Car Trading Platform</h3>
+                    <a href='https://github.com/BlagoKolev/AutoMarket' className='btn' target='_blank'>View on Github</a>
+                </article>
+
+                <article className='portfolio__item'>
+                    <div className='portfolio__item-image'>
+                        <img src={forum} alt='forum project image'></img>
+                    </div>
+                    <h3>Forum System</h3>
+                    <a href='https://github.com/BlagoKolev/MyForumSystem' className='btn' target='_blank'>View on Github</a>
+                </article>
+
+                <article className='portfolio__item'>
+                    <div className='portfolio__item-image'>
+                        <img src={bookshelf} alt='bookshelf project image'></img>
+                    </div>
+                    <h3>Books Sharing Platform</h3>
+                    <a href=' https://github.com/BlagoKolev/Bookshelf' className='btn' target='_blank'>View on Github</a>
+                </article>
+               
+                <article className='portfolio__item'>
+                    <div className='portfolio__item-image'>
+                        <img src={quiz} alt='quiz project image'></img>
+                    </div>
+                    <h3>Quiz Game</h3>
+                    <a href='https://github.com/BlagoKolev/Quiz-Books-and-Authors' className='btn' target='_blank'>View on Github</a>
+                </article>
+
+
+            </div>
+        </section>
+    )
 }
 
 export default Portfolio;
